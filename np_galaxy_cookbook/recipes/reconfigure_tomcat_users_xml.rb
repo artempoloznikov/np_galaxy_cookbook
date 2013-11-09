7a5fec6d232770680c22b9464c92f5ad4cccd81e
artempoloznikov <artem.poloznikov@clearscale.net>
2013-11-10 04:11:32 +0700

    +1

---
+++ np_galaxy_cookbook/recipes/reconfigure_tomcat_users_xml.rb
@@ -0,0 +1,12 @@
+template "#{node[:np_galaxy_cookbook][:tomcat][:home]}/etc/tomcat7/tomcat-users.xml" do
+      cookbook "np_galaxy_cookbook"
+      source 'tomcat-users.xml.erb'
+      mode '0640'
+      owner node[:np_galaxy_cookbook][:tomcat][:user]
+      group node[:np_galaxy_cookbook][:tomcat][:group]
+      variables(
+		:tomcat_rolename = > node[:np_galaxy_cookbook][:tomcat_users_xml][:tomcat_rolename],
+		:tomcat_username = > node[:np_galaxy_cookbook][:tomcat_users_xml][:tomcat_username],
+		:tomcat_password = > node[:np_galaxy_cookbook][:tomcat_users_xml][:tomcat_password]
+      )
+end
