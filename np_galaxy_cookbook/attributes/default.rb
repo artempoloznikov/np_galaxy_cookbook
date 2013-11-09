bb9f6ac26ae8ab3761cd5f4f9a1e15ca176d8d29
artempoloznikov <artem.poloznikov@clearscale.net>
2013-11-10 05:09:45 +0700

    +1

--- np_galaxy_cookbook/attributes/default.rb
+++ np_galaxy_cookbook/attributes/default.rb
@@ -17,18 +17,16 @@ default[:np_galaxy_cookbook][:s3_bucket_name] = "np-titan-tomcat-install"
 
 default[:np_galaxy_cookbook][:java_instdir] = "/usr/java"
 
-default[:np_galaxy_cookbook][:tomcat][:home] = "/usr/local/tomcat-core"
-default[:np_galaxy_cookbook][:tomcat][:tmp] = "/usr/local/tomcat-core/tmp"
-default[:np_galaxy_cookbook][:tomcat][:pid_file] = "/usr/local/tomcat-core/tmp/tomcat.pid"
+default[:np_galaxy_cookbook][:tomcat][:home] = "/mnt/ephemeral/tomcat7"
+default[:np_galaxy_cookbook][:tomcat][:tmp] = "/tmp"
+default[:np_galaxy_cookbook][:tomcat][:pid_file] = "/var/run/tomcat7.pid"
 default[:np_galaxy_cookbook][:tomcat][:logs] = "/mnt/ephemeral/tomcat-logs"
-default[:np_galaxy_cookbook][:tomcat][:user] = "tomcat"
-default[:np_galaxy_cookbook][:tomcat][:group] = "tomcat"
+default[:np_galaxy_cookbook][:tomcat][:user] = "tomcat7"
+default[:np_galaxy_cookbook][:tomcat][:group] = "tomcat7"
 default[:np_galaxy_cookbook][:tomcat][:java_xms] = "256m"
 default[:np_galaxy_cookbook][:tomcat][:java_xmx] = "1200m"
 
 default[:np_galaxy_cookbook][:tomcat_xml][:tomcat_port] = "8080"
-default[:np_galaxy_cookbook][:tomcat_xml][:tomcat_port] = "8080"
-
 
 default[:np_galaxy_cookbook][:tomcat_users_xml][:tomcat_rolename] = "manager-gui"
 default[:np_galaxy_cookbook][:tomcat_users_xml][:tomcat_username] = "admin"
