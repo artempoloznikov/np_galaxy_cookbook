a8802113d8b859c42e630536cea5100b9779d59c
artempoloznikov <artem.poloznikov@clearscale.net>
2013-11-10 17:23:58 +0700

    +1

--- np_galaxy_cookbook/metadata.rb
+++ np_galaxy_cookbook/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "artempoloznikov@clearscale.net"
 license          "None"
 description      "Cookbook provides tomcat application server implementation."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "13.5.13"
+version          "13.5.14"
 
 #supports "centos"
 #supports "redhat"
@@ -37,7 +37,7 @@ attribute "np_galaxy_cookbook/tomcat/rolename",
   :required => "optional",
   :default => "manager-gui",
   :recipes => [
-    "np_galaxy_cookbook::reconfigure_tomcat_users_xml",
+    "np_galaxy_cookbook::reconfigure_tomcat_users_xml"
   ]
 
 attribute "np_galaxy_cookbook/tomcat/username",
@@ -46,7 +46,7 @@ attribute "np_galaxy_cookbook/tomcat/username",
   :required => "optional",
   :default => "admin",
   :recipes => [
-    "np_galaxy_cookbook::reconfigure_tomcat_users_xml",
+    "np_galaxy_cookbook::reconfigure_tomcat_users_xml"
   ]
 
 attribute "np_galaxy_cookbook/tomcat/password",
@@ -55,7 +55,7 @@ attribute "np_galaxy_cookbook/tomcat/password",
   :required => "optional",
   :default => "***",
   :recipes => [
-    "np_galaxy_cookbook::reconfigure_tomcat_users_xml",
+    "np_galaxy_cookbook::reconfigure_tomcat_users_xml"
   ]
 
 attribute "np_galaxy_cookbook/tomcat/user",
@@ -64,7 +64,7 @@ attribute "np_galaxy_cookbook/tomcat/user",
   :required => "optional",
   :default => "tomcat7",
   :recipes => [
-    "np_galaxy_cookbook::reconfigure_tomcat_users_xml",
+    "np_galaxy_cookbook::reconfigure_tomcat_users_xml"
   ]
 
 attribute "np_galaxy_cookbook/tomcat/group",
@@ -73,7 +73,7 @@ attribute "np_galaxy_cookbook/tomcat/group",
   :required => "optional",
   :default => "tomcat7",
   :recipes => [
-    "np_galaxy_cookbook::reconfigure_tomcat_users_xml",
+    "np_galaxy_cookbook::reconfigure_tomcat_users_xml"
   ]
 
 #######
@@ -84,7 +84,7 @@ attribute "np_galaxy_cookbook/netpulse_properties/env",
   :required => "optional",
   :default => "dev",
   :recipes => [
-    "np_galaxy_cookbook::reconfigure_netpulse_properties",
+    "np_galaxy_cookbook::reconfigure_netpulse_properties"
   ]
 
 attribute "np_galaxy_cookbook/netpulse_properties/workout_summary_test_mode",
@@ -93,27 +93,105 @@ attribute "np_galaxy_cookbook/netpulse_properties/workout_summary_test_mode",
   :required => "optional",
   :default => "dev",
   :recipes => [
-    "np_galaxy_cookbook::reconfigure_netpulse_properties",
+    "np_galaxy_cookbook::reconfigure_netpulse_properties"
   ]
 
-attribute "np_galaxy_cookbook/netpulse_properties/env",
+attribute "np_galaxy_cookbook/netpulse_properties/workout_summary_job_status",
   :display_name => "workout_summary_job_status",
   :description => "workout summary job status",
   :required => "optional",
   :default => "off",
   :recipes => [
-    "np_galaxy_cookbook::reconfigure_netpulse_properties",
+    "np_galaxy_cookbook::reconfigure_netpulse_properties"
   ]
 
+attribute "np_galaxy_cookbook/netpulse_properties/site_host",
+  :display_name => "site_host",
+  :description => "site host",
+  :required => "optional",
+  :default => "https://dev-api.netpulse.com",
+  :recipes => [
+    "np_galaxy_cookbook::reconfigure_netpulse_properties"
+  ]
 
-#default[:np_galaxy_cookbook][:netpulse_properties][:site_host] = "https://dev-api.netpulse.com"
+attribute "np_galaxy_cookbook/netpulse_properties/memcached_serverList",
+  :display_name => "memcached_serverList",
+  :description => "memcached.serverList",
+  :required => "optional",
+  :default => "dev-app-1.netpulse.ws:11211,dev-app-2.netpulse.ws:11211",
+  :recipes => [
+    "np_galaxy_cookbook::reconfigure_netpulse_properties"
+  ]
 
-#default[:np_galaxy_cookbook][:netpulse_properties][:memcached.serverList] = "dev-app-1.netpulse.ws:11211,dev-app-2.netpulse.ws:11211"
-#default[:np_galaxy_cookbook][:netpulse_properties][:embeddedActivemq_dataDirectory] = "/mnt/tomcat/activemq_galaxy_data"
-#default[:np_galaxy_cookbook][:netpulse_properties][:workout_image_path] = "/mnt/tomcat/workout_image_data"
-#default[:np_galaxy_cookbook][:netpulse_properties][:workout_error_path] = "/mnt/tomcat/workout_error_data"
-#default[:np_galaxy_cookbook][:netpulse_properties][:alerts_workoutErrorThreshold] = "15"
-#default[:np_galaxy_cookbook][:netpulse_properties][:workout_manualCsDistributionList] = "ncasuga@netpulse.com"
-#default[:np_galaxy_cookbook][:netpulse_properties][:workout_error_distributionList] = "ncasuga@netpulse.com"
-#default[:np_galaxy_cookbook][:netpulse_properties][:email_enabled] = "true"
-#default[:np_galaxy_cookbook][:netpulse_properties][:partner_fitstudio_key] = "secret_key"
+attribute "np_galaxy_cookbook/netpulse_properties/embeddedActivemq_dataDirectory",
+  :display_name => "embeddedActivemq_dataDirectory",
+  :description => "embeddedActivemq_dataDirectory",
+  :required => "optional",
+  :default => "/mnt/tomcat/activemq_galaxy_data",
+  :recipes => [
+    "np_galaxy_cookbook::reconfigure_netpulse_properties"
+  ]
+
+attribute "np_galaxy_cookbook/netpulse_properties/workout_image_path",
+  :display_name => "workout_image_path",
+  :description => "workout_image_path",
+  :required => "optional",
+  :default => "/mnt/tomcat/workout_image_data",
+  :recipes => [
+    "np_galaxy_cookbook::reconfigure_netpulse_properties"
+  ]
+
+attribute "np_galaxy_cookbook/netpulse_properties/workout_error_path",
+  :display_name => "workout_error_path",
+  :description => "workout_error_path",
+  :required => "optional",
+  :default => "/mnt/tomcat/workout_error_data",
+  :recipes => [
+    "np_galaxy_cookbook::reconfigure_netpulse_properties"
+  ]
+
+attribute "np_galaxy_cookbook/netpulse_properties/alerts_workoutErrorThreshold",
+  :display_name => "alerts_workoutErrorThreshold",
+  :description => "alerts_workoutErrorThreshold",
+  :required => "optional",
+  :default => "15",
+  :recipes => [
+    "np_galaxy_cookbook::reconfigure_netpulse_properties"
+  ]
+
+attribute "np_galaxy_cookbook/netpulse_properties/workout_manualCsDistributionList",
+  :display_name => "workout_manualCsDistributionList",
+  :description => "workout_manualCsDistributionList",
+  :required => "optional",
+  :default => "ncasuga@netpulse.com",
+  :recipes => [
+    "np_galaxy_cookbook::reconfigure_netpulse_properties"
+  ]
+
+attribute "np_galaxy_cookbook/netpulse_properties/",
+  :display_name => "workout_error_distributionList",
+  :description => "workout_error_distributionList",
+  :required => "optional",
+  :default => "ncasuga@netpulse.com",
+  :recipes => [
+    "np_galaxy_cookbook::reconfigure_netpulse_properties"
+  ]
+
+attribute "np_galaxy_cookbook/netpulse_properties/email_enabled",
+  :display_name => "email_enabled",
+  :description => "email_enabled",
+  :required => "optional",
+  :default => "true",
+  :recipes => [
+    "np_galaxy_cookbook::reconfigure_netpulse_properties"
+  ]
+
+attribute "np_galaxy_cookbook/netpulse_properties/partner_fitstudio_key",
+  :display_name => "partner_fitstudio_key",
+  :description => "partner_fitstudio_key",
+  :required => "optional",
+  :default => "secret_key",
+  :recipes => [
+    "np_galaxy_cookbook::reconfigure_netpulse_properties"
+  ]
+
