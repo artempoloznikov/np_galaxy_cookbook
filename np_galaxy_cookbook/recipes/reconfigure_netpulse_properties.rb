976c9c7b0ba8c3cb376369d34294031f3a6c9717
artempoloznikov <artem.poloznikov@clearscale.net>
2013-11-12 02:02:34 +0700

    update

--- np_galaxy_cookbook/recipes/reconfigure_netpulse_properties.rb
+++ np_galaxy_cookbook/recipes/reconfigure_netpulse_properties.rb
@@ -5,19 +5,19 @@ template "#{node[:np_galaxy_cokbook][:tomcat][:conf]}/netpulse.properties" do
       owner node[:np_galaxy_cookbook][:tomcat][:user]
       group node[:np_galaxy_cookbook][:tomcat][:group]
       variables(
-            :tomcat_env = node[:np_galaxy_cookbook][:netpulse_properties][:env],
-            :tomcat_workout_summary_test_mode = node[:np_galaxy_cookbook][:netpulse_properties][:workout_summary_test_mode],
-            :tomcat_workout_summary_job_status = node[:np_galaxy_cookbook][:netpulse_properties][:workout_summary_job_status],
-            :tomcat_site_host = node[:np_galaxy_cookbook][:netpulse_properties][:site_host],
-            :tomcat_memcached_serverList = node[:np_galaxy_cookbook][:netpulse_properties][:memcached_serverList],
-            :tomcat_embeddedActivemq_dataDirectory = node[:np_galaxy_cookbook][:netpulse_properties][:embedded_activemq_data_directory],
-            :tomcat_workout_image_path = node[:np_galaxy_cookbook][:netpulse_properties][:workout_image_path],
-            :tomcat_workout_error_path = node[:np_galaxy_cookbook][:netpulse_properties][:workout_error_path],
-            :tomcat_alerts_workoutErrorThreshold = node[:np_galaxy_cookbook][:netpulse_properties][:alerts_workout_error_threshold],
-            :tomcat_workout_manualCsDistributionList = node[:np_galaxy_cookbook][:netpulse_properties][:workout_manual_cs_distribution_list],
-            :tomcat_workout_error_distributionList = node[:np_galaxy_cookbook][:netpulse_properties][:workout_error_distribution_list],
-            :tomcat_email_enabled = node[:np_galaxy_cookbook][:netpulse_properties][:email_enabled],
-            :tomcat_partner_fitstudio_key = node[:np_galaxy_cookbook][:netpulse_properties][:partner_fitstudio_key]
+            :tomcat_env => node[:np_galaxy_cookbook][:netpulse_properties][:env],
+            :tomcat_workout_summary_test_mode => node[:np_galaxy_cookbook][:netpulse_properties][:workout_summary_test_mode],
+            :tomcat_workout_summary_job_status => node[:np_galaxy_cookbook][:netpulse_properties][:workout_summary_job_status],
+            :tomcat_site_host => node[:np_galaxy_cookbook][:netpulse_properties][:site_host],
+            :tomcat_memcached_serverList => node[:np_galaxy_cookbook][:netpulse_properties][:memcached_serverList],
+            :tomcat_embeddedActivemq_dataDirectory => node[:np_galaxy_cookbook][:netpulse_properties][:embedded_activemq_data_directory],
+            :tomcat_workout_image_path => node[:np_galaxy_cookbook][:netpulse_properties][:workout_image_path],
+            :tomcat_workout_error_path => node[:np_galaxy_cookbook][:netpulse_properties][:workout_error_path],
+            :tomcat_alerts_workoutErrorThreshold => node[:np_galaxy_cookbook][:netpulse_properties][:alerts_workout_error_threshold],
+            :tomcat_workout_manualCsDistributionList => node[:np_galaxy_cookbook][:netpulse_properties][:workout_manual_cs_distribution_list],
+            :tomcat_workout_error_distributionList => node[:np_galaxy_cookbook][:netpulse_properties][:workout_error_distribution_list],
+            :tomcat_email_enabled => node[:np_galaxy_cookbook][:netpulse_properties][:email_enabled],
+            :tomcat_partner_fitstudio_key => node[:np_galaxy_cookbook][:netpulse_properties][:partner_fitstudio_key]
 
       )
 end
--- np_galaxy_cookbook/recipes/reconfigure_tomcat_users_xml.rb
+++ np_galaxy_cookbook/recipes/reconfigure_tomcat_users_xml.rb
@@ -1,4 +1,4 @@
-template "#{node[:np_galaxy_cokbook][:tomcat][:conf]}/tomcat-users.xml" do
+template "#{node[:np_galaxy_cookbook][:tomcat][:conf]}/tomcat-users.xml" do
       cookbook "np_galaxy_cookbook"
       source 'tomcat-users.xml.erb'
       mode '0640'
